a5ca14ac0abb762c1ee6c03102ae7a09d76fd5d2
Denis Lavrushko <dlavrushko@protonmail.com>
2021-09-23 16:58:29 +0300

    Test ci (#2)

---
+++ .github/actions/comment.js
@@ -0,0 +1,21 @@
+const github = require('@actions/core');
+
+async function run() {
+    const { issue_number, github_token, owner, repo, website_link } = process.env
+
+    const octokit = new github.GitHub(github_token);
+
+    try {
+        const { data } = await octokit.issues.createComment({
+            owner,
+            repo,
+            issue_number,
+            body: `[Review website here](${website_link.replace(/"/gs, '')})`
+        });
+
+    } catch (err) {
+        throw err
+    }
+}
+
+run();
